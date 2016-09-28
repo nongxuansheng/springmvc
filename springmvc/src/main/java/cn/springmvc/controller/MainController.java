077e6f57d9440915b6b38ae3c9ac6fe4ce5be8ef
Mangon <Mangon@mangon.com>
2016-09-28 16:01:52 +0800

    springMVC基本架构，首次提交。

---
+++ springmvc/src/main/java/cn/springmvc/controller/MainController.java
@@ -0,0 +1,15 @@
+package cn.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+	
+	@RequestMapping("index")
+	public String index(){
+		System.out.println(111);
+		return "index";
+	}	
+}
---
+++ springmvc/src/main/java/cn/springmvc/dao/UserDAO.java
@@ -0,0 +1,14 @@
+package cn.springmvc.dao;
+
+import cn.springmvc.model.User;
+
+
+public interface UserDAO {
+	/**
+	 * 添加新用户
+	 * @param user
+	 * @return
+	 */
+	public int insertUser(User user);	
+	
+}
